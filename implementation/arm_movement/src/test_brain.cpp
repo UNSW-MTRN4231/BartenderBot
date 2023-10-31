7e5fc3525ed60819263eb1ba1c0da987202ffc79
BenCrimp <z5310078@ad.unsw.edu.au>
2023-11-01 10:57:54 +1100

    test brain for no transform testing

--- implementation/arm_movement/src/test_brain.cpp
+++ implementation/arm_movement/src/test_brain.cpp
@@ -16,25 +16,22 @@
 #include "tf2_ros/buffer.h"
 
 using std::placeholders::_1;
+using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
-class arm_brain : public rclcpp::Node {
+class test_brain : public rclcpp::Node {
   public:
-    arm_brain() : Node("arm_brain")
+    test_brain() : Node("test_brain")
     {
-      // Initalise the brain subscriber
-      subscription_ = this->create_subscription<brain_msgs::msg::Command>("command", 10, std::bind(&arm_brain::brain, this, _1));
-
       // Initialise the pose publisher
       pose_publisher_ = this->create_publisher<geometry_msgs::msg::Pose>("move_to", 10);
 
-      // Initialise the brain ready publisher
-      ready_publisher_ = this->create_publisher<std_msgs::msg::String>("ready", 10);
-
       // Initialise the arduino publisher
       arduino_publisher_ = this->create_publisher<std_msgs::msg::String>("arduino", 10);
+      //timer
+      timer_ = this->create_wall_timer(5000ms, std::bind(&test_brain::brain, this));
 
       // Initialise current arm pose
       curr_pose.position.x = 0;
@@ -51,42 +48,6 @@ class arm_brain : public rclcpp::Node {
     }
 
   private:
-    //Function to generate a target position message
-    geometry_msgs::msg::Pose generatePoseFromTransform(geometry_msgs::msg::TransformStamped tf) {
-      geometry_msgs::msg::Pose msg;
-    
-      msg.position.x = tf.transform.translation.x;
-      msg.position.y = tf.transform.translation.y;
-      msg.position.z = tf.transform.translation.z;
-      
-      // TODO: CHECK ROTATION OF GRIPPER FOR PICKUPS
-      msg.orientation.x = tf.transform.rotation.x;
-      msg.orientation.y = tf.transform.rotation.y;
-      msg.orientation.z = tf.transform.rotation.z;
-      msg.orientation.w = tf.transform.rotation.w;
-      
-      return msg;
-    }
-
-    //requests transform frame for object
-    geometry_msgs::msg::TransformStamped tfCallback(std::string req_frame) {
-      // Check if the transformation is between "world" and "req_frame"
-      std::string fromFrameRel = "world";
-      std::string toFrameRel = req_frame;
-      geometry_msgs::msg::TransformStamped t;
-
-      try {
-          t = tf_buffer_->lookupTransform( toFrameRel, fromFrameRel, tf2::TimePointZero);
-      } catch (const tf2::TransformException & ex) {
-          RCLCPP_INFO( this->get_logger(), "Could not transform %s to %s: %s", toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
-          
-      }
-    }
-
-    //gets pose of object
-    geometry_msgs::msg::Pose get_pose(std::string item_frame) {
-        return generatePoseFromTransform(tfCallback(item_frame));
-    }
     
     //sends pose to ur
     void send_pose() {
@@ -124,19 +85,6 @@ class arm_brain : public rclcpp::Node {
       send_pose();
     }
 
-    void move(std::string item) {
-      
-      if (item == "return") {
-        curr_pose = old_pose;
-      }
-      else {
-        curr_pose = get_pose(item);
-      }
-      
-      send_pose();
-
-      //TODO: CHECK IF OBJECT IS STILL IN LOCATION -> MOVE AGAIN
-    }
 
     // combines both parts of shaker and mixes, then dissasembles
     void shake(geometry_msgs::msg::Pose big, geometry_msgs::msg::Pose small) {
@@ -219,72 +167,30 @@ class arm_brain : public rclcpp::Node {
 
     
 
-    void brain(const brain_msgs::msg::Command &msg) {
+    void brain() {
       
       //moves to home
       home();
       grip(0);
       // check for what command is
       
-      // adding ingredient
-      if (msg.command.data == "fill") {
-        //need repeat
-        for (auto i = 0; i < size(msg.item_frames); i++) {
-          move(msg.item_frames[i].data); // to bottle
-          old_pose = curr_pose;
-          grip(1);  
-          move("big_shaker"); //to shaker
-          pour(msg.item_heights[i].data);
-          move("return"); //to old bottle;
-          grip(0); //release
-        }
-      }
-      
-      // mixing ingredients
-      else if (msg.command.data == "shake") {
-        shake(get_pose("big_shaker"), get_pose(msg.item_frames[0].data));
-      }
-      
-      // pouring cocktail
-      else if (msg.command.data == "pour") {
-        
-        move("big_shaker"); //to shaker
-        old_pose = curr_pose;
-        grip(1);
-          
-        //need repeat
-        for (auto i = 0; i < size(msg.item_frames); i++) {
-          move(msg.item_frames[i].data);
-          pour(msg.item_heights[i].data);
-        }
-
-        move("return"); //to old shaker
-        grip(0); //release
-      }
-      else {
-        RCLCPP_INFO( this->get_logger(), "Unknown Command");
-      }
-      std_msgs::msg::String ready;
-      ready.data  = "ready";
-      ready_publisher_->publish(ready);
     }
 
     std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
 
-    rclcpp::Subscription<brain_msgs::msg::Command>::SharedPtr subscription_;
     rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr pose_publisher_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr ready_publisher_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr arduino_publisher_;
     geometry_msgs::msg::Pose curr_pose;
     geometry_msgs::msg::Pose old_pose;
+    rclcpp::TimerBase::SharedPtr timer_;
     size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<arm_brain>());
+  rclcpp::spin(std::make_shared<test_brain>());
   rclcpp::shutdown();
   return 0;
 }
