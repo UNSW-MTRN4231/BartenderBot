a5e59c1e5577d1894ccef3fab5f10373d1e8e277
BenCrimp <z5310078@ad.unsw.edu.au>
2023-11-08 10:00:08 +1100

    moved current fns to arm brain

--- implementation/arm_movement/src/arm_brain.cpp
+++ implementation/arm_movement/src/arm_brain.cpp
@@ -118,10 +118,12 @@ class arm_brain : public rclcpp::Node {
       if (toggle == 0) {
         grip.data  = "open";
         toggle = 1;
+        RCLCPP_INFO(this->get_logger(), "Claw Open");
       }
       else if(toggle == 1) {
         grip.data = "close";
         toggle = 0;
+        RCLCPP_INFO(this->get_logger(), "Claw Close");
       }
       arduino_publisher_->publish(grip);
     }
@@ -156,8 +158,10 @@ class arm_brain : public rclcpp::Node {
       //TODO: CHECK IF OBJECT IS STILL IN LOCATION -> MOVE AGAIN
     }
 
+    // moves to and picks up object
     void pickup(geometry_msgs::msg::Pose pose) {
       tf2::Quaternion q;
+      RCLCPP_INFO(this->get_logger(), "Starting pickup");
       if (pose.position.y > 0.3) {
         curr_pose.position.y = pose.position.y - 0.15;
         q.setRPY(M_PI/2 ,-M_PI/2 , M_PI);
@@ -166,7 +170,7 @@ class arm_brain : public rclcpp::Node {
         q.setRPY(M_PI, -M_PI/2 , -M_PI/2);
       }
       curr_pose.position.x = pose.position.x;
-      curr_pose.position.z = pose.position.z;
+      curr_pose.position.z = pose.position.z + 0.07;
       
       curr_pose.orientation.x = q.x();
       curr_pose.orientation.y = q.y();
@@ -178,34 +182,38 @@ class arm_brain : public rclcpp::Node {
       send_pose("linear");
       grip(1);
 
-      curr_pose.position.z = 0.3;
+      curr_pose.position.z = 0.37;
       send_pose("linear");
     }
 
     // combines both parts of shaker and mixes, then dissasembles
     void shake(geometry_msgs::msg::Pose big, geometry_msgs::msg::Pose small) {
       // picks up small shaker, moves above other, rotates and combines
-      curr_pose = small;
-      send_pose();
-      grip(1);
-      curr_pose.position.z += 20;
-      send_pose();
-      curr_pose = big;
-      curr_pose.position.z += 20;
-      send_pose();
+      
+      pickup(small);
+      tf2::Quaternion q;
+      q.setRPY(M_PI, 0 , M_PI/2);
+      curr_pose.orientation.x = q.x();
+      curr_pose.orientation.y = q.y();
+      curr_pose.orientation.z = q.z();
+      curr_pose.orientation.w = q.w();
 
-      curr_pose.position.z -= 10;
-      send_pose();
+      curr_pose.position.x = big.position.x;
+      curr_pose.position.y = big.position.y;
+      send_pose("linear");
+
+      curr_pose.position.z -= 0.15;
+      send_pose("linear");
       grip(0);
       // picks up combined, moves above and rotates
-      curr_pose.position.z -= 5;
-      send_pose();
+      curr_pose.position.z -= 0.05;
+      send_pose("linear");
       grip(1);
 
-      curr_pose.position.z += 30;
-      send_pose();
+      curr_pose.position.z += 0.3;
+      send_pose("linear");
 
-      tf2::Quaternion q;
+    
       q.setRPY(M_PI, 0 , M_PI/2);
       curr_pose.orientation.x = q.x();
       curr_pose.orientation.y = q.y();
@@ -220,46 +228,71 @@ class arm_brain : public rclcpp::Node {
       send_pose();
 
       // places back down and dissasembles
-      curr_pose.position.z -= 30;
-      send_pose();
+      curr_pose.position.z -= 0.3;
+      send_pose("linear");
       grip(0);
 
-      curr_pose.position.z += 5;
-      send_pose();
+      curr_pose.position.z += 0.05;
+      send_pose("linear");
       grip(1);
 
-      curr_pose.position.z += 10;
+      curr_pose.position.z += 0.10;
+      send_pose("linear");
+
+      q.setRPY(M_PI, 0 , M_PI/2);
+      curr_pose.orientation.x = q.x();
+      curr_pose.orientation.y = q.y();
+      curr_pose.orientation.z = q.z();
+      curr_pose.orientation.w = q.w();
       send_pose();
 
-      curr_pose.position.x += 15;
-      send_pose();
+      curr_pose.position.x = small.position.x;
+      curr_pose.position.y = small.position.y;
+      send_pose("linear");
+      curr_pose.position.z = small.position.z;
+      send_pose("linear");
       grip(0);
     }
     
     // pours the bottle at current location
     void pour(float offset) {
-        offset = offset/2;
-        // moves accross offset width
-        curr_pose.position.x += offset;
-        send_pose();
+      offset = (0.07-offset/2)/sqrt(2);
         
-        // pours drink
-        tf2::Quaternion q;
-        q.setRPY(M_PI/4, 0 , 0); // 45 degrees roll in rad
-        curr_pose.orientation.x = q.x();
-        curr_pose.orientation.y = q.y();
-        curr_pose.orientation.z = q.z();
-        curr_pose.orientation.w = q.w();
-        send_pose();
-        curr_pose.orientation.x = 0;
-        curr_pose.orientation.y = 0;
-        curr_pose.orientation.z = 0;
-        curr_pose.orientation.w = 0;
-        send_pose();
+      tf2::Quaternion q;
+      q.setRPY(-M_PI/2, -M_PI/2 , M_PI/2); // facing computer
+      curr_pose.orientation.x = q.x();
+      curr_pose.orientation.y = q.y();
+      curr_pose.orientation.z = q.z();
+      curr_pose.orientation.w = q.w();
+      send_pose();
+      RCLCPP_INFO(this->get_logger(), "Moving to pour angle");
+      if (curr_pose.position.y > 0.3) {
+        // moves accross offset width
+        curr_pose.position.y = curr_pose.position.y - offset;
+        send_pose("linear");
+        q.setRPY(-M_PI/2, M_PI/4 , M_PI/2); // pour towards
+      } else {
+        // moves accross offset width
+        curr_pose.position.y = curr_pose.position.y + offset;
+        send_pose("linear");
+        q.setRPY(M_PI/2, M_PI/4 , -M_PI/2); // pour away
+      }
+      // pours drink
+      curr_pose.orientation.x = q.x();
+      curr_pose.orientation.y = q.y();
+      curr_pose.orientation.z = q.z();
+      curr_pose.orientation.w = q.w();
+      send_pose();
+      RCLCPP_INFO(this->get_logger(), "Pouring...");
+      sleep(2);
+      
 
-        // returns to original position
-        curr_pose.position.x -=offset;
-        send_pose();
+      q.setRPY(-M_PI/2, -M_PI/2 , M_PI/2); // return to upright
+      curr_pose.orientation.x = q.x();
+      curr_pose.orientation.y = q.y();
+      curr_pose.orientation.z = q.z();
+      curr_pose.orientation.w = q.w();
+      send_pose();
     }
 
     
